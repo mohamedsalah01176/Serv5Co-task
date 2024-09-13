c0b4234076c4b51e045a377278d4c0b1387d1da8
Mohamed Salah <118165638+mohamedsalah01176@users.noreply.github.com>
2024-09-13 10:59:10 +0300

    Update Title.tsx

--- app/component/Title.tsx
+++ app/component/Title.tsx
@@ -10,7 +10,7 @@ export default function Title({title}:props) {
   return (
     <div data-aos="zoom-in" data-aos-duration="1000" className='text-center mt-10 mb-10 relative'>
       <span className=' text-5xl sm:text-7xl  font-bold text-[#eee]'>{title}</span>
-      <span className='absolute text-3xl sm:text-5xl  font-semibold   top-[25%] left-[50%] translate-x-[-50%]'>{title}</span>
+      <h1 className='absolute text-3xl sm:text-5xl  font-semibold   top-[25%] left-[50%] translate-x-[-50%]'>{title}</span>
     </div>
   )
 }
